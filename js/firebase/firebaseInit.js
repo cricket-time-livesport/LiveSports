1b785f06ca5a0d9e83dfd95fd9a710a183fa0242
Maulik <Maulikkumbhani23@Icloud.com>
2023-10-16 16:55:49 +0530

    Update firebase

--- js/firebase/firebaseInit.js
+++ js/firebase/firebaseInit.js
@@ -1,38 +1,22 @@
-      // Your web app's Firebase configuration
-      const firebaseConfig = {
-        apiKey: "AIzaSyDd9HRMo58f8lg3vysPZTsCS18tOr5qNz4",
-        authDomain: "cricketlive-e61c8.firebaseapp.com",
-        projectId: "cricketlive-e61c8",
-        storageBucket: "cricketlive-e61c8.appspot.com",
-        messagingSenderId: "137047518182",
-        appId: "1:137047518182:web:8010bb14cea003db7c0427",
-        measurementId: "G-5R68V0PNYP",
-        databaseURL:"https://cricketlive-e61c8-default-rtdb.firebaseio.com"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-      var firebaseRef = firebase.database().ref().child('data');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-      //Flegs Store
-      var showAds = false
-      
-      firebaseRef.once("value").then(function(snapshot){
-        var loginInfo=snapshot.val();
-        showAds = loginInfo.showAds
-      });
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDd9HRMo58f8lg3vysPZTsCS18tOr5qNz4",
+  authDomain: "cricketlive-e61c8.firebaseapp.com",
+  databaseURL: "https://cricketlive-e61c8-default-rtdb.firebaseio.com",
+  projectId: "cricketlive-e61c8",
+  storageBucket: "cricketlive-e61c8.appspot.com",
+  messagingSenderId: "137047518182",
+  appId: "1:137047518182:web:8010bb14cea003db7c0427",
+  measurementId: "G-5R68V0PNYP"
+};
 
-
-      var button = document.getElementById("watchLiveBtn");
-      button.addEventListener("click", function(event){
-        debugger
-            alert(event.target);
-         if (showAds == true){
-            document.getElementById('score-frame').hidden = false
-         }else{
-            document.getElementById('score-frame').hidden = true
-         }
-      }); 
-
-      
-
-    
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
